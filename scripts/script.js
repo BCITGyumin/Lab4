a571a605b34a9b1aa11d80ffa1acf70db751c2e9
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-07 14:58:04 -0800

    get 4-digit number from the user

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-const courseList = [{code:"ACIT-1420", name:"Introduction to Systes Administration"}, {code:"ACIT-1620", name:"Web Fundamental Technologies"}, {code:"ACIT-1630", name:"Databse Systems"}];
+const courseList = 
+[
+    {code:"ACIT-1420", name:"Introduction to Systes Administration"},
+    {code:"ACIT-1620", name:"Web Fundamental Technologies"},
+    {code:"ACIT-1630", name:"Databse Systems"}
+];
+
+let user
+do{
+user = prompt("Please Enter a 4-digit number of the course code");
+}
+while(isNaN(user) || !(user.length === 4));
+
