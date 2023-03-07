8f495ddd05fcb4854f30fcd41ff7732fdd981258
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-07 13:57:44 -0800

    define courseList array in script.js

---
+++ scripts/script.js
@@ -0,0 +1,1 @@
+const courseList = [{code:"ACIT-1420", name:"Introduction to Systes Administration"}, {code:"ACIT-1620", name:"Web Fundamental Technologies"}, {code:"ACIT-1630", name:"Databse Systems"}];
