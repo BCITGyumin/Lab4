df19afc0f32103263b658e9bd01da4d2b2b0d27b
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-07 15:48:56 -0800

    course checker

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,28 @@ const courseList =
     {code:"ACIT-1630", name:"Databse Systems"}
 ];
 
+
+//  Get 4-digit Number from the user
 let user
 do{
 user = prompt("Please Enter a 4-digit number of the course code");
 }
 while(isNaN(user) || !(user.length === 4));
 
+
+//  Check the course in the array
+let courseFound = false;
+for (let i = 0; i < courseList.length; i++) {
+    if (courseList[i].code.substring(5) === user) {
+        console.log(`Yes I am taking the course: ${courseList[i].code} - ${courseList[i].name}`);
+        courseFound = true;
+        break;
+    }
+}
+
+if (!courseFound) {
+    courseList.push({code:`ACIT-${user}`, name:null});
+    console.log(`Course with code ACIT-${user} has been added to the list.`);
+}
+
+console.log(courseList);
